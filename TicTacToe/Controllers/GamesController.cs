4dd726d8f4d596333056c72f992af394307e843b
IngaSam <Kayumovainga2@mail.ru>
2025-07-12 15:20:01 +0300

    Initial project setup with Docker

---
+++ TicTacToe/Controllers/GamesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicTacToe.Models;
+
+namespace TicTacToe.Controllers
+{
+    [ApiController]
+    [Route("games")]
+    public class GamesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateGame()
+        {
+            return Ok(new { message = "Game created!" });
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetGame(Guid id) 
+        {
+            return Ok(new { id = id });
+        }
+        [HttpPost("{id}/moves")]
+        public IActionResult MakeMove(Guid id, [FromBody] MoveRequest move)
+        {
+            return Ok(new { move = move });
+        }
+
+
+    }
+}
---
+++ TicTacToe/Models/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace TicTacToe.Models
+{
+    public class Game
+    {
+        public Guid Id { get; set; }
+        public string[,] Board { get; set; }
+        public string CurrentPlayer { get; set; }
+        public string? Winner { get; set; }
+        public int MoveCount { get; set; }
+    }
+}
